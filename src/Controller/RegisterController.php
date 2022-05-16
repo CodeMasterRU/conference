e6426e1b43110a1f60f690b94b086016f59633f5
CodeMasterRU <vitaly.skvortsov@ynov.com>
2022-05-16 16:13:31 +0200

    register_page

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -7,14 +7,40 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\RegisterType;
 use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RegisterController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this -> entityManager = $entityManager;
+    }
+
     #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this -> createForm(RegisterType::class, $user);
+
+        $form -> handleRequest($request);
+
+        if($form -> isSubmitted() && $form -> isValid()){
+
+            $user = $form -> getData();
+
+            $passwordHashed = $passwordHasher -> hashPassword($user, $user -> getPassword());
+            $user -> setPassword($passwordHashed);
+
+            $this -> entityManager -> persist($user);
+            $this -> entityManager -> flush();
+        }
+
+
         
         return $this->render('register/index.html.twig', [
             'form' => $form -> createView()
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class RegisterType extends AbstractType
 {
@@ -14,9 +21,59 @@ class RegisterType extends AbstractType
 
         
         $builder
-            ->add('email')
-            ->add('password')
-        ;
+        ->add('firstname', TextType::class, [
+            'label'=> "FIRSTNAME",
+            'constraints' => new Length([
+                'min' => 2,
+                'max' => 30
+            ]),
+            'attr' => [
+                'placeholder' => 'votre firstname'
+            ]
+        ])
+        ->add('lastname', TextType::class, [
+            'label' => "LASTNAME",
+            'constraints' => new Length([
+                'min' => 2,
+                'max' => 30
+            ]),
+            'attr' => [
+                'placeholder' => 'votre lastname'
+            ]
+        ]) 
+        ->add('email', EmailType::class, [
+            'constraints' => new Length([
+                'min' => 2,
+                'max' => 30
+            ]),
+            'label' => "Email",
+            'attr' => [
+                'placeholder' => 'votre mail'
+                ]
+        ])
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'non indentique',
+            'label' => "PASSWD",
+            'required' => true,
+            'first_options' => [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'votre mot de passe'
+                ]
+            ],
+            'second_options' => [
+                'label' => 'Confirmer votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'confirmer votre mot de passe'
+                ]
+            ],
+            
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => "S'inscrire",
+        ])
+    ; 
     }
 
     public function configureOptions(OptionsResolver $resolver): void
