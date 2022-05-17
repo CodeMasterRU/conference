26a59f456f6558c4612fa4d317c5ba8c67f55034
CodeMasterRU <vitaly.skvortsov@ynov.com>
2022-05-17 13:07:07 +0200

    admin final et les div d'inscription et d'connexion

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -124,4 +124,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->getLastname();
+    } 
 }
